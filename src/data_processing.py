3585a737202d9cfa3a7d2d31131c3ba72ef57415
baspresso <baspresso@gmail.com>
2024-12-15 07:48:04 +0300

    TODO tasks

---
+++ src/data_processing.py
@@ -0,0 +1,12 @@
+# TODO: Реализовать функцию выделение фичей датасета
+import pandas as pd
+
+def process_data(orders: pd.DataFrame, purchases: pd.DataFrame, products: pd.DataFrame)-> pd.DataFrame:
+    """
+    Обрабатывает данные из трех DataFrame (orders, purchases, products)
+    и возвращает обработанный DataFrame с выделенными признаками.
+
+    Returns:
+    - Результирующий DataFrame с выделенными признаками.
+    """
+    pass
---
+++ src/data_selectoion.py
@@ -0,0 +1,5 @@
+# TODO: Реализовать функцию выделение товаров, на которых будет обучаться модель
+import pandas as pd
+
+def select_data(data: pd.DataFrame)-> pd.DataFrame:
+    pass
